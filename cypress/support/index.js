869980466cdfb20577b9c3fe6678cb885a334c98
Filip Hric <filip@filiphric.com>
2022-03-23 09:06:56 +0100

    remove unused packages

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,6 @@
 import '../../workshop-scripts/testSetupHook'
 import '../../workshop-scripts/infoCommand'
 
-import '@bahmutov/cy-api'
-import "cypress-real-events/support";
-
 Cypress.Commands.add('dataCy', (selector) => {
 
   cy.get(`[data-cy=${selector}]`)
